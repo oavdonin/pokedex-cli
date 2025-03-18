08bcb05008f5dbf731b2245fc88dee3287c3e052
oavdonin <9444556+oavdonin@users.noreply.github.com>
2025-03-18 14:55:56 +0100

    Trying to catch up

--- main.go
+++ main.go
@@ -1,95 +1,15 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-
-	"github.com/oavdonin/pokedex-cli/api"
+	"time"
+	"github.com/oavdonin/pokedex-cli"
 )
 
-type cliCommand struct {
-	name        string
-	description string
-	callback    func() error
-}
-
-var commandSet = map[string]cliCommand{
-	"help": {
-		name:        "help",
-		description: "Displays a help message",
-		callback:    commandHelp,
-	},
-	"map": {
-		name:        "map",
-		description: "List locations",
-		callback:    pokedexMap,
-	},
-	"mapb": {
-		name:        "mapb",
-		description: "",
-		callback:    pokedexMapb,
-	},
-	"exit": {
-		name:        "exit",
-		description: "exit",
-		callback:    func() error { return nil },
-	},
-}
-
-func printPrompt() error {
-	fmt.Print("pokedex > ")
-	return nil
-}
-
-func commandVersion() error {
-	fmt.Println("version 0.1")
-	return nil
-}
-
-func commandHelp() error {
-	fmt.Println("available commands: [version, help, exit]")
-	return nil
-}
-
-func pokedexMap() error {
-	api.InitializeLocations()
-	err := api.GetLocations("next")
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func pokedexMapb() error {
-	api.InitializeLocations()
-	err := api.GetLocations("back")
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-	return nil
-}
-
 func main() {
-
-	reader := bufio.NewScanner(os.Stdin)
-	printPrompt()
-replloop:
-	for reader.Scan() {
-		command := reader.Text()
-		switch commandSet[command].name {
-		case "exit":
-			break replloop
-		case "help":
-			commandHelp()
-		case "map":
-			pokedexMap()
-		case "mapb":
-			pokedexMapb()
-		default:
-			commandHelp()
-		}
-		printPrompt()
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapiClient: pokeClient,
 	}
+
+	startRepl(cfg)
 }
